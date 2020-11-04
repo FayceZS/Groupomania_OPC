a3c8b45e7e8a1acdccc6bbf9181655412acd0ee2
FayceZS <fayssalibrahimi@gmail.com>
2020-11-04 18:19:39 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,59 +7,9 @@ const bodyParser = require('body-parser');          //On va utiliser bodyParser 
 const db = require("./models");
 const { User} = require('./models/user');
 
-app.get('/select',(req,res) => {
-  res.send("select");
-});
-
-app.get('/insert',(req,res) => {
-  // User.create(
-  //          { 
-  //           prenom : "Pedro" ,
-  //           nom : "Rodriguez",
-  //           sexe : "m",
-  //           fonction : "tacos",
-  //           mail : "PRODRI@gmail.com",
-  //           password : "password"
-  //         }
-  // )
-  // .catch(err => {
-  //   if(err){
-  //     console.log(err)
-  //   }
-  // })
-});
 
 
-db.sequelize.sync().then((req)=> {
-app.listen(3001, () => {
-  console.log('server running');
-})
-});
 
-
-const connectToDb  = (req,res,next)=>{ 
-
-const con= mysql.createConnection ({             //On se connecte à notre base de données mySQL
-  host: 'localhost',
-  user: 'root',
-  password: 'Fayssal02',
-  database : "groupomania" 
-  
- });
-con.connect ((err) => {
-  if (err) throw err;
-  console.log ('Connecté!');
-});  
-
-con.end ((err) => {
-  // La connexion se termine normalement
-  // Garantit que toutes les requêtes restantes sont exécutées
-  // Envoie ensuite un paquet de sortie au serveur MySQL.
-  if (err) throw err;
-  console.log("Connexion réussie");
-}); 
-
-  }; 
 
 
   app.use((req, res, next) => {                                                         //On définie nos CORS pour donner l'accès du backend au frontend
@@ -73,7 +23,6 @@ app.use(bodyParser.json());
 app.use('/auth',userRoutes);
 
 
-connectToDb();
 
 
 
